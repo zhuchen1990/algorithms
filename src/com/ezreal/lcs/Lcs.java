4888deca84a979be128715774fe57bd9f436cc80
Ezreal <564699585@qq.com>
2018-05-08 20:02:59 +0800

    Lcs and QuickSort commit and push

--- src/com/ezreal/lcs/Lcs.java
+++ src/com/ezreal/lcs/Lcs.java
@@ -1,7 +1,77 @@
 package com.ezreal.lcs;
 
+
 /**
- * Created by Ezreal# on 2018/5/4.
+ * @author Ezreal
+ * @version 2018/5/4.
+ * @deprecated 最长子序列问题
  */
 public class Lcs {
+    public static void main(String[] args) {
+        String[] x ={"A", "B", "C", "B", "D", "A", "B"};
+        String[] y ={"B", "D", "C", "A", "B", "A"};
+        lcslength(x,y);
+    }
+
+    private static void lcslength(String[] x, String[] y){
+        int m = x.length+1;
+        int n = y.length+1;
+        char[][]b = new char[m][n];
+        int[][]c = new int[m][n];
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++){
+                if (x[i-1].equals(y[j-1])){
+                    c[i][j] = c[i-1][j-1] + 1;
+                    b[i][j] = '↖';
+                }else if (c[i-1][j] >= c[i][j-1]){
+                    c[i][j] = c[i-1][j];
+                    b[i][j] = '↑';
+                }else {
+                    c[i][j] = c[i][j-1];
+                    b[i][j] = '←';
+                }
+            }
+        }
+        System.out.print("\t");
+        for (int i= 0;i<y.length;i++){
+            System.out.print(y[i]+"\t");
+        }
+        print(b);
+        System.out.println();
+        print(c);
+        System.out.println();
+        print(b,x,x.length,y.length);
+
+    }
+
+    private static void print(int[][]a){
+        for (int i = 0;i< a.length;i++){
+            for (int j = 0; j<a[i].length;j++){
+                System.out.print(a[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+    private static void print(char[][]a){
+        for (int i = 0;i< a.length;i++){
+            for (int j = 0; j<a[i].length;j++){
+                System.out.print(a[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void print(char[][] b, String[] x, int i, int j){
+        if (i == 0 || j == 0){
+            return;
+        }
+        if (b[i][j] == '↖'){
+            print(b,x,i-1,j-1);
+            System.out.print(x[i-1]+"\t");
+        }else if (b[i][j] == '↑'){
+            print(b,x,i-1,j);
+        }else {
+            print(b,x,i,j-1);
+        }
+    }
 }
---
+++ src/com/ezreal/quciksort/QuickSort.java
@@ -0,0 +1,49 @@
+package com.ezreal.quciksort;
+
+import java.util.Arrays;
+
+/**
+ * @author Ezreal
+ * @version 2018/4/24.
+ * @deprecated 快速排序
+ */
+public class QuickSort {
+
+
+    public static void main(String[] args) {
+        int[] A = new int[]
+                { 1, 12, 2, 13, 3, 14, 4, 15, 5, 16, 17, 17, 177, 18, 8, 8, 19 };
+        QuickSort quicksort = new QuickSort();
+        quicksort.quickSort(A, 0,A.length-1);
+        System.out.println(Arrays.toString(A));
+    }
+
+    private void quickSort(int[] A, int p, int r){
+        if (p > r)
+            return;
+        int q = partition(A,p,r);
+        quickSort(A,p,q-1);
+        quickSort(A,q+1,r);
+
+    }
+
+    private Integer partition(int[] A, int p, int r){
+        int x = A[r];
+        int i = p - 1;
+        for (int j = p;j < r;j++ ){
+            if (A[j] <= x){
+                i = i+1;
+                exchange(A,i,j);
+            }
+        }
+        exchange(A,i+1,r);
+        return i+1;
+    }
+
+    private void exchange(int[] A,int p,int r){
+        int temp = A[p];
+        A[p] = A[r];
+        A[r] = temp;
+    }
+
+}
